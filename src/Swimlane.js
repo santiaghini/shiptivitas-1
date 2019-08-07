da28f5b4a0ae216f19b2b20d4da0d5494b5aca1f
santiaghini <santi.hergut@gmail.com>
2019-08-07 00:34:14 -0500

    add Dragula functionality

--- src/Swimlane.js
+++ src/Swimlane.js
@@ -3,25 +3,25 @@ import Card from './Card';
 import './Swimlane.css';
 
 export default class Swimlane extends React.Component {
-  render() {
-    const cards = this.props.clients.map(client => {
-      return (
-        <Card
-          key={client.id}
-          id={client.id}
-          name={client.name}
-          description={client.description}
-          status={client.status}
-        />
-      );
-    })
-    return (
-      <div className="Swimlane-column">
-        <div className="Swimlane-title">{this.props.name}</div>
-        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
-          {cards}
-        </div>
-      </div>);
-  }
-
+	render() {
+		const cards = this.props.clients.map((client) => {
+			return (
+				<Card
+					key={client.id}
+					id={client.id}
+					name={client.name}
+					description={client.description}
+					status={client.status}
+				/>
+			);
+		});
+		return (
+			<div className="Swimlane-column">
+				<div className="Swimlane-title">{this.props.name}</div>
+				<div className="Swimlane-dragColumn" ref={this.props.dragulaRef} status={this.props.status}>
+					{cards}
+				</div>
+			</div>
+		);
+	}
 }
